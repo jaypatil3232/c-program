0d3a262ac83578d7716ba62f8b160fc9392bf726
jaypatil3232 <jaywardhanpatil1@gmail.com>
2024-10-26 22:36:25 +0530

    equal.c

---
+++ equal.c
@@ -0,0 +1,20 @@
+//Program to find if the value is equal
+
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2;
+    num1 = 100;
+    num2 = 20; 
+    if (num1 == num2) 
+    {
+        printf("%d is equal %d\n", num1, num2);
+    } 
+    else
+    {
+        printf("%d is not equal %d\n", num1, num2);
+    }
+
+    return 0;
+}
